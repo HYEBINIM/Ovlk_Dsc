acb298e69afb82878f7b4013e430e769ad1dbc0d
HyeBin <ihb3470@kakao.com>
2025-02-04 13:55:10 +0900

    250204 로직 구현 완료
    test.py 로직 구현 완료

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ try:
 
                     # data9 값이 1일 때 plc_data_lh에 한 행 추가
                     insert_query = "INSERT INTO plc_data_lh (date, time) VALUES (%s, %s)"
-                    print(f"Executing query: {insert_query} with values: ({date_value}, {time_value})")
+                    print(f"[LH 추가] {insert_query} [값] {date_value}, {time_value}")
                     
                     try:
                         cursor.execute(insert_query, (date_value, time_value))
@@ -62,9 +62,9 @@ try:
                         # MAX(id) 가져오기
                         cursor.execute("SELECT MAX(id) FROM plc_data_lh")
                         last_lh_id = cursor.fetchone()[0]
-                        print(f"plc_data_lh 에 추가됨: {last_lh_id}")
+                        print(f"[LH에 추가됨] {last_lh_id}")
                     except MySQLdb.Error as err:
-                        print(f"Insert Error: {err}")
+                        print(f"[LH 추가에러] {err}")
 
                 # data9가 문자열 "2"일 때
                 elif data9_value == "2":
@@ -75,7 +75,7 @@ try:
 
                     # data9 값이 2일 때 plc_data_rh에 한 행 추가
                     insert_query = "INSERT INTO plc_data_rh (date, time) VALUES (%s, %s)"
-                    print(f"Executing query: {insert_query} with values: ({date_value}, {time_value})")
+                    print(f"[RH 추가] {insert_query} [값] {date_value}, {time_value}")
                     
                     try:
                         cursor.execute(insert_query, (date_value, time_value))
@@ -85,28 +85,32 @@ try:
                         # MAX(id) 가져오기
                         cursor.execute("SELECT MAX(id) FROM plc_data_rh")
                         last_rh_id = cursor.fetchone()[0]
-                        print(f"plc_data_rh에 추가됨: {last_rh_id}")
+                        print(f"[RH에 추가됨] {last_rh_id}")
                     except MySQLdb.Error as err:
-                        print(f"Insert Error: {err}")
+                        print(f"[RH 추가에러] {err}")
 
                 # data9가 1이나 2가 아닐 경우
                 else:
-                    print("data9의 값이 1이나 2가 아닙니다. 업데이트를 중지합니다.")
+                    print("[작업 정지상태]")
                     previous_data9_value = data9_value  # 이전 값을 업데이트하여 반복에서 계속 확인할 수 있도록
                     last_inserted_table = None  # 마지막 추가된 테이블 초기화
                     continue  # 업데이트 중단
 
                 # 이전 data9 값을 현재 값으로 업데이트
                 previous_data9_value = data9_value
-                print(f"data9의 현재 값: {data9_value}")
+                print(f"[data9] {data9_value}")
 
-            # data9가 바뀌지 않았을 경우, 마지막 추가된 행 업데이트
-            if last_inserted_table is not None:
+        # data9가 바뀌지 않았을 경우, 마지막 추가된 행 업데이트
+        if last_inserted_table is not None:
+            # guide1의 data3, data4, data5 값이 모두 0인지 확인
+            if data3_value == "0" and data4_value == "0" and data5_value == "0":
+                print("[피크치 없음] 업데이트 건너뜀")
+            else:
                 if last_inserted_table == 'plc_data_lh':
                     # 현재 data22, data23, data24 값을 가져오기
                     cursor.execute(f"SELECT data22, data23, data24 FROM {last_inserted_table} WHERE id = {last_lh_id};")
                     current_values = cursor.fetchone()
-            
+                
                     # 새로운 값을 추가할 리스트
                     updated_values = []
                     
@@ -116,32 +120,31 @@ try:
                             updated_values.append(new_value)
                         else:
                             updated_values.append(f"{current_value},{new_value}")
-            
+                
                     update_query = f"""
                     UPDATE {last_inserted_table} 
                     SET data22 = %s, data23 = %s, data24 = %s 
                     WHERE id = {last_lh_id};
                     """
-                    print(f"업데이트 테이블 : {last_inserted_table} 값 : ({updated_values[0]}, {updated_values[1]}, {updated_values[2]})")
+                    print(f"[LH 테이블 업데이트] {last_inserted_table} [값] {updated_values[0]}, {updated_values[1]}, {updated_values[2]}")
                     try:
                         cursor.execute(update_query, tuple(updated_values))
                         conn.commit()  # 수동으로 커밋
-
+        
                         # guide1 테이블 업데이트
                         guide_update_query = """
                         UPDATE guide1 
                         SET data3 = %s, data4 = %s, data5 = %s, contents1 = %s 
                         WHERE id = 2;
                         """
-                        print(f"guide1 업데이트 쿼리 실행: {guide_update_query} with values: (0, 0, 0, 10)")
+                        print(f"[guide1 id2 업데이트] {guide_update_query}")
                         cursor.execute(guide_update_query, (0, 0, 0, 10))
                         conn.commit()  # 즉시 커밋
-                        print("guide1 테이블의 id=2 행이 업데이트되었습니다.")
-
-
-                        # print(f"마지막 업데이트 테이블 : {last_inserted_table}.")
+                        print("[guide1 id2 업데이트 성공]")
+        
                     except MySQLdb.Error as err:
-                        print(f"업데이트 에러 : {err}")
+                        print(f"LH 업데이트 에러] {err}")
+        
             
                 elif last_inserted_table == 'plc_data_rh':
                     # 현재 data22, data23, data24 값을 가져오기
@@ -163,7 +166,7 @@ try:
                     SET data22 = %s, data23 = %s, data24 = %s 
                     WHERE id = {last_rh_id};
                     """
-                    print(f"업데이트 테이블 : {last_inserted_table} 값 : ({updated_values[0]}, {updated_values[1]}, {updated_values[2]})")
+                    print(f"[RH 테이블 업데이트] {last_inserted_table} [값] {updated_values[0]}, {updated_values[1]}, {updated_values[2]}")
                     try:
                         cursor.execute(update_query, tuple(updated_values))
                         conn.commit()  # 수동으로 커밋
@@ -174,15 +177,15 @@ try:
                         SET data3 = %s, data4 = %s, data5 = %s, contents1 = %s 
                         WHERE id = 2;
                         """
-                        print(f"guide1 업데이트 쿼리 실행: {guide_update_query} with values: (0, 0, 0, 10)")
+                        print(f"[guide1 id2 업데이트] {guide_update_query}")
                         cursor.execute(guide_update_query, (0, 0, 0, 10))
                         conn.commit()  # 즉시 커밋
-                        print("guide1 테이블의 id=2 행이 업데이트되었습니다.")
+                        print("[guide1 id2 업데이트 성공]")
 
 
                         # print(f"마지막 업데이트 테이블 : {last_inserted_table}.")
                     except MySQLdb.Error as err:
-                        print(f"업데이트 에러: {err}")
+                        print(f"[RH 업데이트 에러] {err}")
             
 
         # 커서와 연결 종료
@@ -193,11 +196,16 @@ try:
         time.sleep(1)
 
 except KeyboardInterrupt:
-    print("프로그램이 종료되었습니다.")
+    print("====프로그램 종료====")
 
 finally:
     # 마지막 커넥션 종료
     if 'cursor' in locals():
         cursor.close()
-    if 'conn' in locals():
-        conn.close()
+    if conn is not None:
+        try:
+            conn.close()
+        except MySQLdb.Error as err:
+            print(f"[커넥션 종료 에러] {err}")
+
+
